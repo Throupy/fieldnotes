d9244712a4dec93cbf8d2357056c20b92aca39db
owen <owenthroup@gmail.com>
2025-04-04 11:07:53 +0100

    some re-styling on the settings modal

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -3,10 +3,12 @@ import {
   UserIcon,
   PaletteIcon,
   RefreshCcwIcon,
-  CogIcon,
+  Settings2Icon,
   BellIcon,
   KeyIcon,
   UsersIcon,
+  SquareArrowUpRightIcon,
+  CogIcon
 } from "lucide-react";
 import { useAuth } from "../contexts/AuthContext";
 import AccountSettings from "./settings/AccountSettings";
@@ -14,6 +16,7 @@ import PreferenceSettings from "./settings/PreferenceSettings";
 import ConnectionSettings from "./settings/ConnectionSettings";
 import { DialogContent } from "../components/ui/dialog";
 import PeopleSettings from "./settings/PeopleSettings";
+import UserProfilePicture from "./UserProfilePicture";
 
 const SettingsModal = () => {
   const [activeSection, setActiveSection] = useState("User");
@@ -22,15 +25,16 @@ const SettingsModal = () => {
 
   const sections = {
     Account: [
-      { name: "User", icon: <UserIcon strokeWidth={1} /> },
-      { name: "Preferences", icon: <CogIcon strokeWidth={1} /> },
-      { name: "Notifications", icon: <BellIcon strokeWidth={1} /> },
+      { name: user?.username, icon: <UserProfilePicture user={user} className="w-6 h-6" />,},
+      { name: "Preferences", icon: <Settings2Icon strokeWidth={1.5} /> },
+      { name: "Notifications", icon: <BellIcon strokeWidth={1.5} /> },
+      { name: "Connections", icon: <SquareArrowUpRightIcon strokeWidth={1.5} />, comingSoon: true },
     ],
     Workspace: [
-      { name: "Appearance", icon: <PaletteIcon strokeWidth={1} /> },
-      { name: "Connection", icon: <RefreshCcwIcon strokeWidth={1} /> },
-      { name: "People", icon: <UsersIcon strokeWidth={1} /> },
-      { name: "Security", icon: <KeyIcon strokeWidth={1} /> },
+      { name: "General", icon: <CogIcon strokeWidth={1.5} /> },
+      { name: "People", icon: <UsersIcon strokeWidth={1.5} /> },
+      { name: "Security", icon: <KeyIcon strokeWidth={1.5} /> },
+      { name: "Connection", icon: <RefreshCcwIcon strokeWidth={1.5} /> },
     ],
   };
 
@@ -39,25 +43,30 @@ const SettingsModal = () => {
       noDefaultWidth
       className="bg-[var(--bg-color)] text-[var(--text-color)] border border-[var(--sidebar-border)] rounded-xl shadow-2xl max-w-6xl w-[90vw] h-[80vh] flex p-0 overflow-hidden"
     >
-      <div className="w-64 bg-[var(--sidebar-bg)] border-r border-[var(--sidebar-border)] p-4">
+      <div className="w-72 bg-[var(--sidebar-bg)] border-r border-[var(--sidebar-border)] p-1">
         <nav className="space-y-0">
           {Object.entries(sections).map(([sectionName, items]) => (
-            <div className="space-y-0.5 mb-4" key={sectionName}>
-              <h3 className="text-sm font-bold text-[var(--muted-text)] mb-2">
+            <div className="mb-4 space-y-0.5" key={sectionName}>
+              <h3 className="px-3 py-2 text-sm font-bold text-[var(--muted-text)]">
                 {sectionName}
               </h3>
               {items.map((item) => (
                 <button
                   key={item.name}
-                  onClick={() => setActiveSection(item.name)}
-                  className={`w-full text-left py-1 px-4 rounded-md flex items-center gap-2 ${
-                    activeSection === item.name
-                      ? "bg-[var(--active-item)] text-[var(--text-color)]"
-                      : "hover:bg-[var(--active-item)]"
+                  onClick={() => setActiveSection(item.name || "Unknown")}
+                  className={`p-3 w-full text-left py-1 rounded-md flex items-center gap-2 ${
+                  activeSection === item.name
+                    ? "bg-[var(--active-item)] text-[var(--text-color)]"
+                    : "hover:bg-[var(--active-item)]"
                   }`}
                 >
                   {item.icon}
                   <span>{item.name}</span>
+                  {item.comingSoon && (
+                    <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300">
+                      Beta
+                    </span>
+                  )}
                 </button>
               ))}
             </div>
